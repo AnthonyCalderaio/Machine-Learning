2f4db38b2fd541c565a67bf424742e85880eacec
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-19 19:42:34 -0400

    added output for the ec2 instance in tf config

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -1,3 +1,8 @@
+output "ec2_instance_ip" {
+  value = aws_instance.your_instance.public_ip
+  description = "The public IP address of the EC2 instance."
+}
+
 provider "aws" {
   region     = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
