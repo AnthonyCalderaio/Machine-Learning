0b7cc4f18deef8c5b5425e8e00411329e8bbf9a3
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-23 21:09:25 -0400

    allow ssh

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -1,5 +1,4 @@
-
-
+ # EC2
 provider "aws" {
   region     = "us-east-1"
   access_key = var.AWS_ACCESS_KEY_ID
@@ -18,10 +17,21 @@ resource "aws_instance" "example" {
   # user_data = "${file("setup.sh")}"
 }
 
-output "ec2_instance_ip" {
-  value = aws_instance.example.public_ip
+# SSH Traffic
+ingress {
+  description = "SSH"
+  from_port   = 0
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 }
+
 
 
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
+
+
+output "ec2_instance_ip" {
+  value = aws_instance.example.public_ip
+}
