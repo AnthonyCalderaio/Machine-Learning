65b06f848b229bfc333a93ac763682c420dcfaf1
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-23 21:55:06 -0400

    try this

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -9,25 +9,27 @@ resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
 
-  # Allow SSH Traffic
-  ingress {
-    description = "SSH"
-    from_port   = 0
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "ExampleInstance"
   }
 
   # Optionally include user_data to run scripts at launch
   # user_data = "${file("setup.sh")}"
-
-  
 }
 
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 
