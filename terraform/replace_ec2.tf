737606a901970e4dfe95d0469aaaf1821144ab24
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-24 18:49:08 -0400

    sdf

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -17,7 +17,7 @@ resource "aws_key_pair" "my_key_pair" {
 resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key_pair.my_key_pair
+  key_name      = aws_key_pair.my_key_pair.key_name
   # key_name      = "04_23_2024_key"
   # Associate with security group allowing SSH traffic
   security_groups = ["allow_ssh"]
