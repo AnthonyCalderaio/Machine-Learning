797b100830aaf50ed60c3e4ee066d248b94798fe
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-24 17:01:28 -0400

    sad

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -32,17 +32,12 @@ resource "aws_instance" "example" {
 }
 
 # VPC resource
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+#resource "aws_vpc" "example" {
+  #cidr_block = "10.0.0.0/16"
 
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-}
+  #enable_dns_hostnames = true
+  #enable_dns_support   = true
+#}
 
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
