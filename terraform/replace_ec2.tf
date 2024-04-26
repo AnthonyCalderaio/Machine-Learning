bdf192a02361b2c057310148082738527c6036e7
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-26 00:47:18 -0400

    try this

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -1,5 +1,3 @@
-## EC2
-
 # terraform import aws_key_pair.04_23_2024_key us-east-1
 
 # Access Key
@@ -9,13 +7,25 @@ provider "aws" {
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
+# Define a new security group allowing all outbound traffic
+resource "aws_security_group" "allow_outbound" {
+  name        = "allow_outbound"
+  description = "Allow all outbound traffic"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create the EC2 instance and associate it with the new security group
 resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
-  #key_name      = aws_key_pair.my_key_pair.key_name
   key_name      = "04_23_2024_key"
-  # Associate with security group allowing SSH traffic
-  security_groups = ["allow_ssh","allow_outbound"]
+  security_groups = ["allow_ssh", aws_security_group.allow_outbound.name]
 
   tags = {
     Name = "ExampleInstance"
@@ -24,7 +34,6 @@ resource "aws_instance" "example" {
   # Optionally include user_data to run scripts at launch
   # user_data = "${file("setup.sh")}"
 }
-
 
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
