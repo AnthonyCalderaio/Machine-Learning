701c6f00c65092ffc6256f677ff18cda75e6db62
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-26 00:52:02 -0400

    asd

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -8,24 +8,25 @@ provider "aws" {
 }
 
 # Define a new security group allowing all outbound traffic
-resource "aws_security_group" "allow_outbound" {
-  name        = "allow_outbound"
-  description = "Allow all outbound traffic"
+#resource "aws_security_group" "allow_outbound" {
+  #name        = "allow_outbound"
+  #description = "Allow all outbound traffic"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+  #egress {
+    #from_port   = 0
+    #to_port     = 0
+    #protocol    = "-1"
+    #cidr_blocks = ["0.0.0.0/0"]
+  #}
+#}
+
 # , aws_security_group.allow_outbound.name
 # Create the EC2 instance and associate it with the new security group
 resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   key_name      = "04_23_2024_key"
-  security_groups = ["allow_ssh", aws_security_group.allow_outbound.name]
+  security_groups = ["allow_ssh", "allow_outbound"]
 
   tags = {
     Name = "ExampleInstance"
