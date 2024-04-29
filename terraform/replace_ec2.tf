6f95e7e182b1688c08524a8f3dc01e43876d38a5
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-04-29 01:40:52 -0400

    try this

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   key_name      = "04_23_2024_key"
-  security_groups = ["allow_ssh","allow_outbound", "allow_inbound", "flask-api-security-group"]
+  security_groups = ["allow_ssh","allow_outbound", "flask-api-security-group"]
 
   tags = {
     Name = "ExampleInstance"
