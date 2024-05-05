d48fd82cc80448f2cef062c853f7490ddcaacfe6
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-05-04 20:25:07 -0400

    hmm

--- terraform/replace_ec2.tf
+++ terraform/replace_ec2.tf
@@ -8,19 +8,19 @@ provider "aws" {
 }
 
 
-#resource "aws_security_group" "example_sg" {
- # name        = "example-security-group"
- # description = "Allow inbound traffic on port 5000"
+ resource "aws_security_group" "example_sg2" {
+   name        = "allow-outbound-1"
+   description = "Allow inbound traffic on port 5000"
 
- # ingress {
-   # from_port   = 5000
-   # to_port     = 5000
-   # protocol    = "tcp"
-  #  cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from all IP addresses. Adjust as needed.
- # }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from all IP addresses. Adjust as needed.
+ }
 
   // Add more ingress rules as necessary
-#}
+}
 
 
 # Create the EC2 instance and associate it with the new security group
@@ -28,7 +28,7 @@ resource "aws_instance" "example" {
   ami           = "ami-04e5276ebb8451442"
   instance_type = "t2.micro"
   key_name      = "04_23_2024_key"
-  security_groups = ["allow_ssh","allow_outbound", "flask-api-security-group", "example-security-group"]
+  security_groups = ["allow_ssh","allow_outbound", "flask-api-security-group", aws_security_group.example_sg2.name]
 
   tags = {
     Name = "AIBrary"
