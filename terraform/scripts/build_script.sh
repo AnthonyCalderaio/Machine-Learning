794f383b69c1e133c6e5c3c6274fdc4c1a2377cf
AnthonyCalderaio <AnthonyCalderaio@gmail.com>
2024-05-08 00:25:59 -0400

    try this

--- terraform/scripts/build_script.sh
+++ terraform/scripts/build_script.sh
@@ -1,20 +1,7 @@
-####################################
-### When EC2 instance is created
+#!/bin/bash
 
-# python3 -m venv venv
-# source venv/bin/activate
-
-### Install pip
-# sudo yum install python3-pip
-
-### Install requirements
-# pip3 install -r requirements.txt
-
-####################################
-
-### SSH into EC2
-# ssh -i Security/AWS/EC2/04_23_2024_key/04_23_2024_key.pem ec2-user@54.167.120.254
-
+# CD into project
+cd ML_API
 
 # Create and activate environment
 python3 -m venv venv &&
